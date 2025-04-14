66ef63807a088ef0c8f1aef1415edc3f944faee7
xyloquack <jastibelz@gmail.com>
2025-04-13 18:18:28 -0700

    Added the ability to track when the player has completed the puzzle based on a CLEAN_THRESHOLD constant that can be changed to reflect how much we want the player to clean before we consider it good enough.

--- upkeep/scripts/draw.glsl
+++ upkeep/scripts/draw.glsl
@@ -3,11 +3,17 @@
 
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
-layout(set = 1, binding = 0) writeonly uniform image2D output_image;
+layout(set = 2, binding = 0, rgba8) readonly uniform image2D solve_image;
+
+layout(set = 1, binding = 0, rgba8) uniform image2D output_image;
 
 layout(set = 0, binding = 0, std430) restrict buffer ParameterBuffer {
     float data[];
 } params;
+
+layout(set = 0, binding = 1, std430) restrict buffer CountBuffer {
+    int cleanCount;
+} count;
 
 void main() {
     float radius = params.data[0];
@@ -25,5 +31,9 @@ void main() {
         if (dist < radius) {
             imageStore(output_image, pixel_coord, vec4(0.0, 0.0, 0.0, 1.0));
         }
+    }
+    if (imageLoad(solve_image, pixel_coord) == vec4(0.0, 0.0, 0.0, 1.0) && imageLoad(output_image, pixel_coord) == vec4(0.0, 0.0, 0.0, 1.0))
+    {
+        atomicAdd(count.cleanCount, 1);
     }
 }
--- upkeep/scripts/drawing_controller.gd
+++ upkeep/scripts/drawing_controller.gd
@@ -2,27 +2,36 @@ extends Node2D
 
 const RADIUS = 20
 const STEP_DISTANCE = 1
-const MAX_STEPS = 10
+const MAX_STEPS = 20
+const CLEAN_THRESHOLD = 0.98
+var DEBUG_MODE = Server.DEBUG_MODE
 
-var viewport
-var width
-var height
+var viewport: Viewport
+var width: int
+var height: int
 var mask_image: Image
 var mask_texture: ImageTexture
-#var sprite
+var solve_image: Image
+var clean_count: int
+var total_dirty: int
 var last_mouse_position = null
-var DirtyObject
+var DirtyObject: Object
 var timer: Timer
 
-var rd
-var shader
-var pipeline
-var storage_buffer
-var uniform_set
-var output_texture
-var texture_uniform_set
+var rd: RenderingDevice
+var shader: RID
+var pipeline: RID
+var storage_buffer: RID
+var uniform_set: RID
+var output_texture: RID
+var mask_uniform_set: RID
+var solve_uniform_set: RID
+var count_set: RID
+var count_storage_buffer: RID
 
 func _ready():
+	clean_count = 0
+	total_dirty = 0
 	viewport = get_viewport()
 	timer = Timer.new()
 	timer.wait_time = 0.1
@@ -34,15 +43,47 @@ func _ready():
 														"width": width, 
 														"fill_color": Color(255, 255, 255)})
 	mask_texture = ImageTexture.create_from_image(mask_image)
-	#sprite = Sprite2D.new()
-	#sprite.texture = mask_texture
-	#sprite.position = Vector2.ZERO
-	#sprite.visible = true
-	#add_child(sprite)
+	
+	var mask_sprite = Sprite2D.new()
+	mask_sprite.texture = mask_texture
+	mask_sprite.position = Vector2(width/2.0,height/2.0)
+	mask_sprite.visible = true
 	DirtyObject = get_node("DirtyObject")
+	var object_copy = DirtyObject.duplicate()
+	object_copy.position = DirtyObject.position + Vector2(width/2.0,height/2.0)
+	object_copy.modulate = Color(0, 0, 0)
+	
+	var subviewport = SubViewport.new()
+	subviewport.size = Vector2(width, height)
+	print(subviewport.size)
+	subviewport.add_child(mask_sprite)
+	subviewport.add_child(object_copy)
+	subviewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
+	
+	add_child(subviewport)
+	
+	await RenderingServer.frame_post_draw
+	
+	var subviewport_texture = subviewport.get_texture()
+	solve_image = subviewport_texture.get_image()
+	solve_image.convert(Image.FORMAT_RGBA8)
+	
+	for x in solve_image.get_width():
+		for y in solve_image.get_height():
+			if solve_image.get_pixel(x,y) == Color(0, 0, 0):
+				total_dirty += 1
+	
+	mask_sprite.queue_free()
+	object_copy.queue_free()
+	subviewport.queue_free()
+	
 	var shader_material = DirtyObject.material
 	shader_material.set_shader_parameter("mask_texture", mask_texture)
 	shader_material.set_shader_parameter("viewport_size", Vector2(width, height))
+	
+	setup_compute_shader()
+
+func setup_compute_shader():
 	rd = RenderingServer.create_local_rendering_device()
 	var shader_file = load("res://scripts/draw.glsl")
 	var shader_spirv = shader_file.get_spirv()
@@ -50,13 +91,20 @@ func _ready():
 	pipeline = rd.compute_pipeline_create(shader)
 	
 	var pba = PackedFloat32Array([]).to_byte_array()
-	storage_buffer = rd.storage_buffer_create((4 + MAX_STEPS * 2) * 4, pba)
+	storage_buffer = rd.storage_buffer_create(((3 + MAX_STEPS) * 2) * 4, pba)
 
 	var uniform_buffer = RDUniform.new()
 	uniform_buffer.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
 	uniform_buffer.binding = 0
 	uniform_buffer.add_id(storage_buffer)
-	uniform_set = rd.uniform_set_create([uniform_buffer], shader, 0)
+	
+	var packed_count_array = PackedInt32Array([clean_count]).to_byte_array()
+	count_storage_buffer = rd.storage_buffer_create(4, packed_count_array)
+	var count_buffer = RDUniform.new()
+	count_buffer.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	count_buffer.binding = 1
+	count_buffer.add_id(count_storage_buffer)
+	uniform_set = rd.uniform_set_create([uniform_buffer, count_buffer], shader, 0)
 
 	var fmt = RDTextureFormat.new()
 	fmt.width = width
@@ -64,19 +112,27 @@ func _ready():
 	fmt.usage_bits = RenderingDevice.TEXTURE_USAGE_STORAGE_BIT | RenderingDevice.TEXTURE_USAGE_CAN_UPDATE_BIT | RenderingDevice.TEXTURE_USAGE_CAN_COPY_FROM_BIT | RenderingDevice.TEXTURE_USAGE_SAMPLING_BIT
 	fmt.format = RenderingDevice.DATA_FORMAT_R8G8B8A8_UNORM
 	output_texture = rd.texture_create(fmt, RDTextureView.new(), [mask_image.get_data()])
+	var solve_texture = rd.texture_create(fmt, RDTextureView.new(), [solve_image.get_data()])
 
-	var texture_uniform = RDUniform.new()
-	texture_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
-	texture_uniform.binding = 0
-	texture_uniform.add_id(output_texture)
-	texture_uniform_set = rd.uniform_set_create([texture_uniform], shader, 1)
+	var mask_uniform = RDUniform.new()
+	mask_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+	mask_uniform.binding = 0
+	mask_uniform.add_id(output_texture)
+	mask_uniform_set = rd.uniform_set_create([mask_uniform], shader, 1)
+	
+	var solve_uniform = RDUniform.new()
+	solve_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+	solve_uniform.binding = 0
+	solve_uniform.add_id(solve_texture)
+	solve_uniform_set = rd.uniform_set_create([solve_uniform], shader, 2)
 
 func _process(_delta):
 	if timer.time_left <= 0.01:
-		print(Engine.get_frames_per_second())
+		#print(Engine.get_frames_per_second())
+		#print(clean_count)
 		timer.start()
-	var point_array = []
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		var point_array = []
 		var mouse_position = get_global_mouse_position()
 		if last_mouse_position == null:
 			last_mouse_position = mouse_position
@@ -90,15 +146,16 @@ func _process(_delta):
 		draw_around_points(point_array, mask_image)
 		mask_texture.update(mask_image)
 		last_mouse_position = mouse_position
+		if check_win():
+			finish_puzzle()
 	else:
 		last_mouse_position = null
 	if point_off_canvas(get_global_mouse_position()):
 		last_mouse_position = null
-	check_win()
 
 func draw_around_points(point_array, image):
-	var x_offset = width / 2
-	var y_offset = height / 2
+	var x_offset = width / 2.0
+	var y_offset = height / 2.0
 	
 	var unpacked_points = []
 	unpacked_points.append(RADIUS)
@@ -109,10 +166,13 @@ func draw_around_points(point_array, image):
 	
 	var pba = PackedFloat32Array(unpacked_points).to_byte_array()
 	rd.buffer_update(storage_buffer, 0, pba.size(), pba)
+	var packed_count_array = PackedInt32Array([0]).to_byte_array()
+	rd.buffer_update(count_storage_buffer, 0, 4, packed_count_array)
 
 	var compute_list = rd.compute_list_begin()
 	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
-	rd.compute_list_bind_uniform_set(compute_list, texture_uniform_set, 1)
+	rd.compute_list_bind_uniform_set(compute_list, solve_uniform_set, 2)
+	rd.compute_list_bind_uniform_set(compute_list, mask_uniform_set, 1)
 	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
 	rd.compute_list_dispatch(compute_list, ceil(width / 8.0), ceil(height / 8.0), 1)
 	rd.compute_list_end()
@@ -120,20 +180,34 @@ func draw_around_points(point_array, image):
 	rd.sync()
 
 	var updated_image_data = rd.texture_get_data(output_texture, 0)
+	var updated_clean_count = rd.buffer_get_data(count_storage_buffer, 0, 4)
+	clean_count = updated_clean_count.decode_s32(0)
 	image.set_data(width, height, false, Image.FORMAT_RGBA8, updated_image_data)
 				
 func point_off_canvas(point):
-	return point.x > width / 2 - 1 or point.x < -width / 2 or point.y < -height / 2 or point.y > height / 2 - 1
+	return point.x > width / 2.0 - 1 or point.x < -width / 2.0 or point.y < -height / 2.0 or point.y > height / 2.0 - 1
 	
 func check_win():
-	pass
+	return clean_count > total_dirty * CLEAN_THRESHOLD
+	
+func finish_puzzle():
+	var complete_label = get_node("Complete")
+	var exit_button = get_node("Exit")
+	complete_label.visible = true
+	exit_button.visible = true
+	
+func _on_exit_button_pressed():
+	if DEBUG_MODE:
+		print("Exit signal received.")
+	var exit_button = get_node("Exit")
+	if exit_button.visible:
+		get_tree().change_scene_to_file("res://scenes/starter_room.tscn")
 	
 func _exit_tree():
 	if rd:
-		rd.free_rid(shader)
-		rd.free_rid(pipeline)
-		rd.free_rid(storage_buffer)
-		rd.free_rid(uniform_set)
-		rd.free_rid(output_texture)
-		rd.free_rid(texture_uniform_set)
-		rd.free_rid(rd)
+		if shader.is_valid():
+			rd.free_rid(shader)
+		if storage_buffer.is_valid():
+			rd.free_rid(storage_buffer)
+		if output_texture.is_valid():
+			rd.free_rid(output_texture)
